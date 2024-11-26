2bf17e2fcd2489e56b3ae9b963c09ea8cf3f7d56
Rune Schuster <runeschuster@gmail.com>
2024-11-26 17:53:02 +0100

    refactor AboutMe.vue; replace static text with dynamic variables for improved maintainability and update styling for better readability

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,20 +1,23 @@
 <template>
   <section id="about">
-    <h2>About me</h2>
-    <p>
-      As a former freelance musician turned software developer, I blend
-creativity with a structured, problem-solving mindset.
-    </p>
+    <h2>{{ sectionTitle }}</h2>
+    <p>{{ sectionText }}</p>
   </section>
 </template>
 
 <style scoped>
 #about {
   padding: var(--section-gap);
-  text-decoration-color:rgba(255, 255, 255, 1.0);
   background-color: rgba(0, 0, 0, 0.8);
   border-radius: 10px;
   margin: 20px;
 }
-
+#about h2, #about p {
+  color: rgba(255, 255, 255, 0.8);
+}
 </style>
+
+<script setup lang="ts">
+const sectionTitle : string = "About me";
+const sectionText : string = "As a former freelance musician turned software developer, I blend creativity with a structured, problem-solving mindset.";
+</script>
